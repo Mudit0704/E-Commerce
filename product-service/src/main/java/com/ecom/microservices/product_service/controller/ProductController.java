4c61e40694795081d598b43106e8987977c5abfb
Mudit0704 <maheshwari7mudit@gmail.com>
2024-11-13 16:30:05 -0500

    Adding order-service base

--- product-service/src/main/java/com/ecom/microservices/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/ecom/microservices/product_service/controller/ProductController.java
@@ -2,7 +2,6 @@ package com.ecom.microservices.product_service.controller;
 
 import com.ecom.microservices.product_service.dto.ProductRequest;
 import com.ecom.microservices.product_service.dto.ProductResponse;
-import com.ecom.microservices.product_service.model.Product;
 import com.ecom.microservices.product_service.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
