16fa1c5c7fc6d471b662b00a7f72bd1137dd0444
Mudit0704 <maheshwari7mudit@gmail.com>
2024-11-18 18:36:58 -0500

    Added Kafka

--- order-service/src/main/java/com/ecom/microservices/order_service/service/OrderService.java
+++ order-service/src/main/java/com/ecom/microservices/order_service/service/OrderService.java
@@ -2,9 +2,11 @@ package com.ecom.microservices.order_service.service;
 
 import com.ecom.microservices.order_service.client.InventoryClient;
 import com.ecom.microservices.order_service.dto.OrderRequest;
+import com.ecom.microservices.order_service.event.OrderPlacedEvent;
 import com.ecom.microservices.order_service.model.Order;
 import com.ecom.microservices.order_service.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -14,6 +16,7 @@ import java.util.UUID;
 public class OrderService {
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest order) {
         boolean isProductInStock = inventoryClient.isInStock(order.skuCode(), order.quantity());
@@ -26,6 +29,14 @@ public class OrderService {
             newOrder.setQuantity(order.quantity());
 
             orderRepository.save(newOrder);
+
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent();
+            orderPlacedEvent.setOrderNumber(newOrder.getOrderNumber());
+            orderPlacedEvent.setEmail(order.userDetails().email());
+            orderPlacedEvent.setFirstName(order.userDetails().firstName());
+            orderPlacedEvent.setLastName(order.userDetails().lastName());
+
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
         } else {
             throw new RuntimeException("Product"+ order.skuCode() +"is not in stock");
         }
